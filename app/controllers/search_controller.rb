4f5d2de045be093a0b3a054d4316bec92b8b81f7
Ryan Martin <rmartin@gilt.com>
2012-01-26 14:44:20 -0500

    Simple search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,17 @@
 class SearchController < ApplicationController
-
   def index
+    @query = params[:query]
 
+    unless @query == ''
+      @results = do_search(@query)
+    end
   end
 
+  def do_search(query)
+    Product.find(:all, :conditions => [
+      "lower(name) like ? or lower(description) like ?",
+      "%#{query}%",
+      "%#{query}%"
+      ])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ ActionController::Routing::Routes.draw do |map|
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
+  map.resources :products
 
   # Sample resource route with options:
   #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
@@ -31,9 +31,10 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "search"
 
   # See how all your routes lay out with "rake routes"
+  map.search 'search', :controller => 'search', :action => 'index'
 
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
