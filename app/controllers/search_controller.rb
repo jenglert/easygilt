9335745c32c9240d778fefdecb8c38803ac67479
Jim Englert <jenglert@gilt.com>
2012-01-26 15:21:09 -0500

    More changes.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,16 +2,15 @@ class SearchController < ApplicationController
   def index
     @query = params[:query]
 
-    unless @query == ''
+    if !@query.nil?  && @query != ''
       @results = do_search(@query)
     end
   end
 
   def do_search(query)
     Product.find(:all, :conditions => [
-      "lower(name) like ? or lower(description) like ?",
-      "%#{query}%",
-      "%#{query}%"
+        "to_tsvector('english', description) @@ to_tsquery('english', ?)",
+        query
       ])
   end
 
